46c1d1db8f270e87c5c108a111531dd16c733b34
Bruna Rocha <bsant4131@gmail.com>
2024-12-15 14:00:46 -0300

    Iniciando o código em python.

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Bem-vindo(a) ao quiz geek rápido!")
+
+jogando = input("VocÊ quer jogar?").lower
+
+if jogando != "sim":
+    print("Jogando")
+else:
+    print("Tudo bem! Talvez outra hora")
+    print("Voltando a tela principal...")
+    quit()
+
